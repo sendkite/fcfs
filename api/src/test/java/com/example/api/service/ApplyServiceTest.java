e7704ff85dbcbad09e5bf5209d210ec3b6467d9f
sendkite <sendkite1@gmail.com>
2023-06-07 23:36:08 +0900

    feat race condition test

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +28,28 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    public void applyManyCouponAtOnce() throws InterruptedException {
+        int threadCount = 1000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드의 작업이 끝날 때까지 기다리는 역할을 하는 클래스
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
